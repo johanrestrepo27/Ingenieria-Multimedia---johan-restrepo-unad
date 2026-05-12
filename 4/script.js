4e9eb6dea836e7058c3d3e9873122abda6ff6fcd
johanrestrepo27 <130107453+johanrestrepo27@users.noreply.github.com>
2026-05-11 20:15:40 -0500

    Delete 4 directory

--- 4/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {
-    const nav = document.querySelector("nav");
-
-    if(window.scrollY > 50){
-        nav.style.background = "#000";
-    } else {
-        nav.style.background = "rgba(0,0,0,0.7)";
-    }
-}); 
